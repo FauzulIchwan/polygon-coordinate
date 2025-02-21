80b6c54c5a8415e671f227b2aa772d1756cb5a63
FauzulIchwan <ichwanowen@gmail.com>
2025-02-21 17:55:58 +0800

    dev-fauzul : +taliwind css & daisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -28,26 +28,25 @@ function App() {
     if (pointerPosition) {
       setPolygonPoints([...polygonPoints, [pointerPosition.x, pointerPosition.y]]);
     }
+    console.log(polygonPoints);
   };
   return (
-    <>
-      <div className="p-4 w-full">
-        <h2 className="text-lg font-bold mb-2">Image Annotator</h2>
-
+    <div className="w-screen h-screen flex ">
+      <div className="p-4 w-full justify-center flex flex-col items-center">
+        <h2 className="text-lg font-bold mb-2 text-center">Anotasi Gambar</h2>
         {/* Input untuk upload gambar */}
-        <input
-          type="file"
+        <input 
+          type="file" 
           accept="image/*"
           onChange={handleImageUpload}
-          ref={fileInputRef}
-          className="mb-2"
-        />
+          ref={fileInputRef} 
+          className="file-input file-input-bordered file-input-accent w-full max-w-xs mb-5" />
 
         {/* Canvas untuk anotasi */}
-        <div className="border border-gray-300 inline-block">
-          <Stage width={600} height={400} onClick={handleCanvasClick}>
+        <div className="flex-grow">
+          <Stage width={800} height={600} onClick={handleCanvasClick} className="border border-gray-300">
             <Layer>
-              {image && <Image image={image} width={600} height={400} />}
+              {image && <Image image={image} width={800} height={600} />}
               <Line
                 points={polygonPoints.flat()} // Konversi ke format Konva
                 stroke="red"
@@ -59,14 +58,14 @@ function App() {
         </div>
 
         {/* Hasil koordinat */}
-        <div className="mt-4">
+        <div className="mt-4 text-wrap w-full">
           <h3 className="text-md font-bold">Hasil Anotasi Polygon:</h3>
-          <pre className="bg-gray-100 p-2">
-            {JSON.stringify({ polygon_points: polygonPoints }, null, 1)}
+          <pre className="p-2 text-wrap">
+            {JSON.stringify({ polygon_points: polygonPoints }, null, 0)}
           </pre>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
